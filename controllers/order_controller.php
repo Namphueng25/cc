f3fe9f143fbcdb7edd1f3fe7ac05d939b93fbd42
Namphueng25 <namphueng.k@ku.th>
2021-10-01 15:56:03 +0700

    Update order_controller.php

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -1,1 +1,30 @@
+<?php class OrderController{
+    public function index()
+    {
+        $order_List=Order::getAll();
+        require_once("./views/order/index_order.php");
+    }
+    public function newOrder(){
+        $staff_List=staff::getAll();
+        $customer_List=customer::getAll();
+        require_once('./views/order/newOrder.php');
+    }
+     public function addOrder()
+     {
+         echo "hh";
+         $id_order_cus=$_GET['id_order_cus'];
+         echo $_GET['id_order_cus'];
+        $date_order=$_GET['date_order'];
+         $fname_staff=$_GET['fname_staff'];
+        $name_customer=$_GET['name_customer'];
+        Order::Add($id_order_cus,$date_order,$fname_staff,$name_customer);
+        OrderController::index();
+    }
+    public function updateForm()
+    {
+        $id_order_cus=$_GET['id_order_cus'];
+       
+    }
 
+}
+?>
