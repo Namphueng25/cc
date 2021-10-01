7a2168287d86a10cd38c888800528257af7a84ad
Namphueng25 <namphueng.k@ku.th>
2021-10-01 17:14:38 +0700

    Update index_order.php

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,1 +1,15 @@
+<table border=1>
+
+<tr><td>id_order</td><td>date</td><td>condition</td><td>id_cus</td><td>Staff_id</td><td>เงื่อนไข</td></tr>
+<?php foreach($order_List as $order)
+{
+        echo "<tr><td>$order->id_order</td><td>$order->date</td>
+        <td>$order->condition</td>
+        <td>$order->id_cus</td>
+        <td>$order->Staff_id</td>
+        <td>$order->เงื่อนไข</td>
+        <td>update</td><td>delete</td></tr>";
+}
+echo "</table>";
+?>
 
